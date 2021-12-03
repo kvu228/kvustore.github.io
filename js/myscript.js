926582c745cb53f72bef8c047e299a077c45de79
kvu228 <63705942+kvu228@users.noreply.github.com>
2021-12-03 16:17:20 +0700

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+
+// Load nav
+$(document).ready(function(){
+	$("#nav-holder").load("nav.html");
+});
+
+// Load contact us
+$(document).ready(function(){
+	$("#contact-section").load("contact.html");
+})
+
+// Load footer
+$(document).ready(function(){
+	$("#footer").load("footer.html");
+
+});
+
+// Load back on top btn
+$(document).ready(function(){
+	$("#back-on-top-btn").load("back-on-top-btn.html");
+});
+
+// Load modals
+$(document).ready(function(){
+	$("#modals-placeholder").load("modals.html");
+})
